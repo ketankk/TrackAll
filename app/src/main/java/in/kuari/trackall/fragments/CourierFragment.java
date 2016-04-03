ecd9182b3e98d45566dd40e82a3fa74e002db4cd
sultan_mirza <ketank90@gmail.com>
2016-04-03 11:52:00 +0530

    Notification icon bug fixed for >=21

--- app/src/main/java/in/kuari/trackall/fragments/CourierFragment.java
+++ app/src/main/java/in/kuari/trackall/fragments/CourierFragment.java
@@ -2,7 +2,12 @@ package in.kuari.trackall.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
@@ -115,8 +120,25 @@ final EditText courierName1=courierName;
         }
     }
 
+
     public void scanSomething() {
-        IntentIntegrator.forFragment(this).initiateScan();
+       /* PendingIntent pendingIntent=PendingIntent.getActivity(getActivity(),0, new Intent(), 0);
+
+        Notification.Builder notification = new Notification.Builder(getActivity());
+        notification.setContentTitle("trackAll")
+                .setContentText("Track your consignment from " + courierName + " AwbNo: ")
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setAutoCancel(true)
+                .setOnlyAlertOnce(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            notification.setColor(getResources().getColor(android.R.color.white));
+
+        }
+
+        NotificationManager notificationManager= (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, notification.build());*/
+       IntentIntegrator.forFragment(this).initiateScan();
     }
 
 }
--- app/src/main/java/in/kuari/trackall/utils/NotificationHandler.java
+++ app/src/main/java/in/kuari/trackall/utils/NotificationHandler.java
@@ -3,13 +3,10 @@ package in.kuari.trackall.utils;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
 import android.util.Log;
 
 import java.util.List;
@@ -50,17 +47,21 @@ public class NotificationHandler extends BroadcastReceiver{
         PendingIntent pendingIntent=PendingIntent.getActivity(context, (int) System.currentTimeMillis(), intent, 0);
 
         if(Build.VERSION.SDK_INT>15) {
-            Notification notification = new Notification.Builder(context)
+            Notification.Builder notification = new Notification.Builder(context)
                     .setContentTitle("trackAll")
                     .setContentText("Track your consignment from " + courierName + " AwbNo: " + awb)
                     .setContentIntent(pendingIntent)
                     .setSmallIcon(R.mipmap.ic_launcher)
                     .setAutoCancel(true)
-                    .setOnlyAlertOnce(true)
-                    .build();
+                    .setOnlyAlertOnce(true);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                notification.setColor(context.getResources().getColor(android.R.color.white));
+
+            }
 
             NotificationManager notificationManager= (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationManager.notify(0,notification);
+            notificationManager.notify(0,notification.build());
             Log.d("service", "created");
 
         }
